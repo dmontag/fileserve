d920bb1e77d287ea67c3c2e6148fc2e9c6f0046b
David Montag <david.montag@neotechnology.com>
2013-06-03 15:53:18 -0700

    Added port arg

--- fileserve.rb
+++ fileserve.rb
@@ -6,8 +6,10 @@ require 'sinatra'
 @public_dir = ARGV[0]
 @user = ARGV[1]
 @pass = ARGV[2]
+@port = ARGV[3].to_i
 
 set :public_folder, @public_dir
+set :port, @port
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   username == @user and password == @pass
